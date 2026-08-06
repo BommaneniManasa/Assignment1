c8cf8a548697a7a7090eac593b85f5db998a910a
Manasa <bommanenimanasa@gmail.com>
2026-08-06 10:11:08 +0530

    Add files via upload

---
+++ DataTypes/Question3/Assignment3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+        foreach (char ch in str)
+        {
+            char newChar=(char)(ch+1);
+            if (char.IsUpper(newChar))
+            {
+                Console.Write(char.ToLower(newChar));
+            }
+            else if (char.IsLower(newChar))
+            {
+                Console.Write(char.ToUpper(newChar));
+            }
+            else
+            {
+                Console.Write(newChar);
+            }
+        }
+    }
+}
+
