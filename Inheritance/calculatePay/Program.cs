4c17e07174ea2db855781b2b411e51a81dbf4149
Manasa <bommanenimanasa@gmail.com>
2026-08-06 12:01:28 +0530

    Add files via upload

---
+++ Inheritance/calculatePay/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+interface IPayable
+{
+    void CalculatePay();
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("CalculatePay() method declared in IPayable interface.");
+    }
+}
