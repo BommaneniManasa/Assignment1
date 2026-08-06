98e946145209e559aedbaeb75276d3164a6f4a69
Manasa <bommanenimanasa@gmail.com>
2026-08-06 14:18:22 +0530

    Add files via upload

---
+++ DataTypes/Question2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str=Console.ReadLine();
+        int letters=0;
+        int digits=0;
+
+        foreach (char ch in str)
+        {
+            if (char.IsLetter(ch))
+            {
+                letters++;
+            }
+            if (char.IsDigit(ch))
+            {
+                digits++;
+            }
+        }
+        Console.WriteLine("Alphabets="+letters);
+        Console.WriteLine("Digits ="+digits);
+    }
+}
+
